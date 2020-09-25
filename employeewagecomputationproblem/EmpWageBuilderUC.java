ad394a8870a898f8b15f46e8aab095cfda8d08b7
shashi-chandel <71683377+shashi-chandel@users.noreply.github.com>
2020-09-25 13:21:18 +0530

    Welcome Message Added

---
+++ employeewagecomputationproblem/EmpWageBuilderUC.java
@@ -0,0 +1,8 @@
+package com.employeewagecomputationproblem;
+
+public class EmpWageBuilderUC {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program!!");
+	}
+
+}
