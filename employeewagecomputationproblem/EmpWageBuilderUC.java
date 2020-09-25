d793a90a96892ab319f57929e4a2c779a63c5373
shashi-chandel <71683377+shashi-chandel@users.noreply.github.com>
2020-09-25 14:15:11 +0530

    Wage Calculated

--- employeewagecomputationproblem/EmpWageBuilderUC.java
+++ employeewagecomputationproblem/EmpWageBuilderUC.java
@@ -9,15 +9,9 @@ public class EmpWageBuilderUC {
 		int empWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			{
-			System.out.println("Employee is Present");
 			empHrs = 8;
-			}
 		else
-			{
-			System.out.println("Employee is Absent");
 			empHrs = 0;
-			}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: " + empWage);
 	}
